40a11ad8401277a1a4521a22b0acfe33311c1bf2
Shubhamsunny99 <shubhamsunny99@gmail.com>
2022-04-19 00:07:11 +0530

    chnages

--- app/routes/restuarant.route.js
+++ app/routes/restuarant.route.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const restuarantController = require("../controllers/restuarant.controller");
+const menuController = require("../controllers/menu.controller");
+const Validation = require('../config/validator');
 
 
 const restuarantsRoutes = express.Router()
 
 // /customer is prefix from app/index.js 
-restuarantsRoutes.post('/add-restuarant', restuarantController.addRestuarants)
+restuarantsRoutes.post('/add-restuarant', Validation.addRestuarantValidation, restuarantController.addRestuarants)
 
 restuarantsRoutes.get('/get-all-restuarant', restuarantController.getAllRestuarants)
 
 restuarantsRoutes.get('/get-individual-restuarant/:id', restuarantController.getIndiviualResturants)
 
-restuarantsRoutes.put('/update-restuarant/:id', restuarantController.updateRestuarants)
+restuarantsRoutes.put('/update-restuarant/:id', Validation.addRestuarantValidation, restuarantController.updateRestuarants)
 
 restuarantsRoutes.put('/delete-restuarant/:id', restuarantController.deleteRestuarants)
+
+
+restuarantsRoutes.post('/add-menu', Validation.addMenuValidation, menuController.addMenus)
+
+restuarantsRoutes.get('/get-all-menu', menuController.getAllMenus)
+
+restuarantsRoutes.get('/get-individual-menu/:id', menuController.getIndiviualMenus)
+
+restuarantsRoutes.put('/update-menu/:id', Validation.addMenuValidation, menuController.updateMenus)
 
 
 module.exports = restuarantsRoutes
